488a7d95242fca334c10793e949a9c7f134abab4
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-11-03 16:20:31 -0400

    Set state for search bar container

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) (
+    super(props);
+
+    this.state = { term: ''};
+  )
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input value=/>
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
